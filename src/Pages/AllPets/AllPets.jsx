bdd5588fb0c5ac5cd92b7e80108dc0073117e36a
farhana988 <farhana744255@gmail.com>
2025-01-17 18:47:46 +0600

    made the delete functional in all pets page

--- src/Pages/AllPets/AllPets.jsx
+++ src/Pages/AllPets/AllPets.jsx
@@ -1,9 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
+
 
 
 const AllPets = () => {
+
     const axiosSecure = useAxiosSecure();
     const {
       data: pets  = [],
@@ -24,6 +28,38 @@ const AllPets = () => {
     if (isLoading) return <div>Loading...</div>;
     if (error) return <div>Error: {error.message}</div>;
   
+
+
+      const handleDelete = async (petId) => {
+        console.log(`Attempting to delete pet with ID: ${petId}`);
+    
+        try {
+          const result = await Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
+          });
+    
+          if (result.isConfirmed) {
+            const response = await axiosSecure.delete(`/pet/${petId}`);
+    
+            if (response.data.deletedCount > 0) {
+              refetch()
+              Swal.fire("Deleted!", "Your pet has been deleted.", "success");
+            }
+          }
+        } catch (error) {
+          console.error("Error while deleting pet:", error);
+          Swal.fire("Error", "An error occurred while deleting the pet.", "error");
+        }
+      };
+
+
+
     return (
         <div className="container mx-auto p-6">
       <h1 className="text-3xl font-semibold mb-6">All Pets</h1>
@@ -69,6 +105,9 @@ const AllPets = () => {
                 </td>
                 <td className="px-4 py-2 space-x-2">
                   <button
+                    onClick={() => {
+                      handleDelete(pet._id);
+                    }}
                     className="px-4 py-2 bg-red-600 text-white rounded-full hover:bg-red-700"
                   >
                     Delete
