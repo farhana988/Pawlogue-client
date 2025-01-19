0590b491fd26bbee431d19c638387a4e490611fe
farhana988 <farhana744255@gmail.com>
2025-01-19 21:24:36 +0600

    about us section designed

--- src/Pages/Home/AboutUs.jsx
+++ src/Pages/Home/AboutUs.jsx
@@ -1,22 +1,38 @@
-import Container  from "../../Components/Reusable/Container";
-
+import Container from "../../Components/Reusable/Container";
+import Heading from "../../Components/Reusable/Heading";
+import bg from "../../assets/images/about.png";
 
 const AboutUs = () => {
-    return (
-       <Container>
-         <div className="py-12 ">
-        <div className="container mx-auto text-center">
-            <h2 className="text-4xl font-bold mb-6">About Us</h2>
-            <p className="text-xl text-gray-700 mb-4">
-                We are a passionate team working to connect loving families with pets in need of a home.
-                Our platform makes pet adoption easy and accessible. Whether you are looking for a cat, dog, rabbit, or other pets,
-                our goal is to help animals find their forever homes.
+  return (
+    <div className="pt-20">
+      <Container>
+        <div
+          className="relative bg-center bg-fixed pb-20"
+          style={{
+            backgroundImage: `url(${bg})`,
+          }}
+        >
+          {/* Overlay */}
+          <div className="absolute inset-0 bg-ivory bg-opacity-40 dark:bg-dCard dark:bg-opacity-60"></div>
+
+          <div className="relative container mx-auto text-center p-8 rounded-lg">
+            <Heading title={"About Us"}></Heading>
+            <p className="text-xl lg:text-2xl text-gray-800 dark:text-gray-300 mb-4 md:px-20
+            font-bold">
+              We are a passionate team working to connect loving families with
+              pets in need of a home. Our platform makes pet adoption easy and
+              accessible. Whether you are looking for a cat, dog, rabbit, or
+              other pets, our goal is to help animals find their forever homes.
             </p>
-            <p className="text-lg text-gray-600">Together, we can make the world a better place for pets and their owners!</p>
+            <p className="md:text-lg  text-gray-700 dark:text-gray-400 font-semibold">
+              Together, we can make the world a better place for pets and their
+              owners!
+            </p>
+          </div>
         </div>
+      </Container>
     </div>
-       </Container>
-    );
+  );
 };
 
 export default AboutUs;
