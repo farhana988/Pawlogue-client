ec995d4cec484db285a6a6bd111dbb74c253df89
farhana988 <farhana744255@gmail.com>
2025-01-25 18:57:29 +0600

    pet category re designed

--- src/Pages/Home/PetsCategorySection/PetsCategory.jsx
+++ src/Pages/Home/PetsCategorySection/PetsCategory.jsx
@@ -2,11 +2,10 @@ import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import Container from "../../../Components/Reusable/Container";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import NoData from "../../../Components/Reusable/NoData";
 import CardSkeleton from "../../../Components/loading/CardSkeleton";
 import Heading from "../../../Components/Reusable/Heading";
-import { FaLocationDot } from "react-icons/fa6";
+import PetsCategoryCard from "./PetsCategoryCard";
 
 const PetsCategory = () => {
   const [selectedCategory, setSelectedCategory] = useState("cat");
@@ -51,8 +50,9 @@ const PetsCategory = () => {
               <button
                 key={category}
                 onClick={() => setSelectedCategory(category)}
-                className={`px-5 lg:px-8 py-2 rounded-full border-2 border-lBtn dark:border-dBtn
-                      lg:text-2xl ${
+                className={`px-3 md:px-5 lg:px-8 py-1 rounded-full border-2 border-lBtn
+                   dark:border-dBtn
+                     text-sm md:text-base lg:text-2xl ${
                         selectedCategory === category
                           ? "bg-lBtn dark:bg-dBtn  text-white"
                           : "bg-gray-200 text-gray-700 dark:bg-dCard dark:text-ivory"
@@ -64,49 +64,13 @@ const PetsCategory = () => {
           </div>
 
           {/* Pets Grid */}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-6">
             {filteredPets.length > 0 ? (
-              filteredPets.slice(0, 3).map((pet) => (
-                <div
-                  key={pet._id}
-                  className="bg-lCard dark:bg-dCard  shadow-lg rounded-lg overflow-hidden 
-                  hover:shadow-xl relative
-                  transition-shadow duration-300"
-                >
-                  <img
-                    src={pet.image}
-                    alt={pet.name}
-                    className="w-full h-40 object-cover"
-                  />
-                  <div className="px-6 py-4">
-                    <h3 className="text-lg font-semibold  mb-2">
-                      {pet.name?.substring(0, 30)}
-                    </h3>
-
-                    {/* age */}
-                    <p
-                      className="absolute text-black font-semibold  px-5 py-1 
-                        rounded-bl-2xl z-10 bg-lBtn dark:bg-dBtn
-                        top-0 right-0"
-                    >
-                      Age: {pet?.age}
-                    </p>
-                    <p className="text-sm lg:text-base flex gap-2 my-2">
-                      <FaLocationDot /> {pet.location?.substring(0, 35)}
-                    </p>
-                    <p className="text-sm mb-5">
-                      {pet.shortDescription?.substring(0, 40)}
-                    </p>
-                    <Link
-                      to={`/petDetails/${pet._id}`}
-                      className="mt-4 font-semibold px-5 py-2 rounded-full
-                text-sm lg:text-base 
-               bg-lBtn dark:bg-dBtn"
-                    >
-                      Details
-                    </Link>
-                  </div>
-                </div>
+              filteredPets.slice(0, 4).map((pet) => (
+                <PetsCategoryCard
+                key={pet._id}
+                pet={pet}
+                ></PetsCategoryCard>
               ))
             ) : (
               <div className=" ">
